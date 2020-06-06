08a557f20fb072cc8a50ee6d4ed0855d741d98e3
bogyuni <bogyuni@hotmail.com>
2020-06-06 13:19:35 +0900

    5.3 컴퍼넌트에 ref 달기

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample';
-// import EventPractice from './EventPractice';
-// import MyComponent from './MyComponent';
-// import './App.css';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
-	// render() {
-	// 	const text = 'hi!';
-	// 	const condition = true;
-	// 	const rHeight = Math.round(Math.random() * 300) + 50;
-	// 	const rWidth = Math.round(Math.random() * 300) + 50;
-	// 	console.log(rWidth, rHeight);
-	// 	const style = {
-	// 		backgroundColor: 'red',
-	// 		border: '2px solid yellow',
-	// 		height: rHeight,
-	// 		width: rWidth,
-	// 		WebkitTransition: 'all',
-	// 	};
-	// 	return (
-	// 		<div className="my-div">
-	// 			{/* Hello world */}
-	// 			<h1>react !!</h1>
-	// 			<h2>{condition ? 'true' : text} React!!!</h2>
-	// 			<p style={style}>
-	// 				{rHeight}, {rWidth}
-	// 			</p>
-	// 			<input type="text" />
-	// 		</div>
-	// 	);
-	// }
-
-	// render() {
-	// 	return <MyComponent name={'Beerboy'} age={4} />;
-	// }
-
-	// render() {
-	// 	return <EventPractice />;
-	// }
-
 	render() {
-		return <ValidationSample />;
+		return (
+			<div>
+				<ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+				<button onClick={() => this.ScrollBox.scrollToBottom()}>Go to bottom</button>
+			</div>
+		);
 	}
 }
 
