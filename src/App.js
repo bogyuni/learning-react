97698d1f8894aa77a685e170941f67301d6e8899
bogyuni <bogyuni@hotmail.com>
2020-06-08 12:47:23 +0900

    6.2 데이터 배열을 컴포넌트 배열로 map하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
-				<button onClick={() => this.ScrollBox.scrollToBottom()}>Go to bottom</button>
-				<p>TEST</p>
+				<IterationSample />
 			</div>
 		);
 	}
