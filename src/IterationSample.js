e1f9f8d00e0edd325686ce125f3ed3bd8c21edb1
bogyuni <bogyuni@hotmail.com>
2020-06-08 13:17:14 +0900

    6.3 key

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 class IterationSample extends Component {
-	render() {
-		const names = ['snowman', 'ice', 'snow', 'wind'];
-		const nameList = names.map((name) => <li>{name}</li>);
+	state = {
+		names: ['snowman', 'ice', 'snow', 'wind'],
+		name: '',
+	};
 
-		return <ul>{nameList}</ul>;
+	handleChange = (e) => {
+		this.setState({
+			name: e.target.value,
+		});
+	};
+
+	handleInsert = () => {
+		this.setState({
+			names: this.state.names.concat(this.state.name),
+			name: '',
+		});
+	};
+
+	handleRemove = (index) => {
+		const { names } = this.state;
+
+		this.setState({
+			// names: [...names.slice(0, index), ...names.slice(index + 1, names.length)],
+			names: names.filter((item, i) => i !== index),
+		});
+	};
+
+	render() {
+		const nameList = this.state.names.map((name, index) => (
+			<li key={index} onDoubleClick={() => this.handleRemove(index)}>
+				{name}
+			</li>
+		));
+
+		return (
+			<div>
+				<input onChange={this.handleChange} value={this.state.name} />
+				<button onClick={this.handleInsert}>Add</button>
+				<ul>{nameList}</ul>
+			</div>
+		);
 	}
 }
 export default IterationSample;
